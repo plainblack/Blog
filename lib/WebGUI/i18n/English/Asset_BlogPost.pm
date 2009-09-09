17988d2947e3c7482e44ee6e35996799944d27ff
Eric Kennedy <ekennedy@ek-laptop.(none)>
2009-09-09 13:44:59 -0500

    added internationalization

---
+++ lib/WebGUI/i18n/English/Asset_BlogPost.pm
@@ -0,0 +1,29 @@
+package WebGUI::i18n::English::Asset_BlogPost;
+
+use strict;
+
+our $I18N = {
+	'assetName' => {
+		message => q|BlogPost|,
+		context => q|Post within a Blog Wobject|,
+		lastUpdated => 0
+	},
+	'adding' => {
+		message => q|Adding a post|,
+		context => q|Add a post to the blog|,
+		lastUpdated => 0
+	},
+	'editing' => {
+		message => q|Editing a post|,
+		context => q|Editing an existing post within the blog|,
+		lastUpdated => 0
+	},
+	'save post' => {
+		message => q|Save|,
+		context => q|Save post|,
+		lastUpdated => 0
+	},
+};
+
+1;
+#vim:ft=perl
