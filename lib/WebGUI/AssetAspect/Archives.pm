6799f6d3465964e5c6c709c9318d83c7eb879553
JT Smith <jt@plainblack.com>
2009-09-09 13:45:10 -0500

    a start

--- lib/WebGUI/AssetAspect/Archives.pm
+++ lib/WebGUI/AssetAspect/Archives.pm
@@ -16,7 +16,62 @@ package WebGUI::AssetAspect::Archives;
 
 use strict;
 use Class::C3;
+use JSON;
+use Tie::IxHash;
 use WebGUI::Exception;
-use WebGUI::Asset;
+use WebGUI::Form;
+use WebGUI::HTML;
+use WebGUI::Utility;
+
+=head1 NAME
+
+Package WebGUI::AssetAspect::Archives
+
+=head1 DESCRIPTION
+
+This is an aspect which allows you to add an interface to traverse descendant archives.
+
+=head1 SYNOPSIS
+
+ use Class::C3;
+ use base qw(WebGUI::AssetAspect::Archives WebGUI::Asset);
+ 
+And then where-ever you would call $self->SUPER::someMethodName call $self->next::method instead.
+
+=head1 METHODS
+
+These methods are available from this class:
+
+=cut
+
+#-------------------------------------------------------------------
+
+=head2 getArchivesYears ( )
+
+Returns a sorted hashreference of archives years and the number of assets in that year.
+
+=cut
+
+sub getArchivesYears {
+	my ($self) = @_;
+	my $session = $self->session;
+    my $date = $session->datetime;
+    my $nextAsset = $self->getLineageIterator(["descendants"],{returnObjects=>1});
+    my %years = ();
+    while (my $asset = $nextAsset->()) {
+        my $year = $date->epochToHuman($asset->get('dateCreated'), "%y");
+        $years{$year}++;
+    }   
+    tie my %sortedYears, 'Tie::IxHash';
+    my @yearKeys = sort keys %sortedYears;
+    foreach my $year (@yearKeys) {
+        $sortedYears{$year} = $years{$year};
+    }
+    return \%sortedYears;
+}
+
+
 
 1;
+
+
